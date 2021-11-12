5c5a64eb4645ea75ed28be8ee49b9947bee3a746
mikecat1024 <akkt.mikecat@outlook.jp>
2021-11-12 21:03:09 +0900

    add odd case

--- js/AddClans.js
+++ js/AddClans.js
@@ -3,25 +3,21 @@ var parent = document.getElementById('form_area');
 var clanname = []
 
 function AddForm(){
-    for(let i = 0; i < 2; i++){
-        var input = document.createElement('li');
-        input.innerHTML = '<input type="text" id="inputform' + FormCount + '" value="Clan' + FormCount +'">';
-        var parent = document.getElementById('form_area');
-        parent.appendChild(input);
-        clanname.push('Clan'+i);
-        FormCount++;
-    }
+    var input = document.createElement('li');
+    input.innerHTML = '<input type="text" id="inputform' + FormCount + '" value="Clan' + FormCount +'">';
+    var parent = document.getElementById('form_area');
+    parent.appendChild(input);
+    clanname.push('Clan'+i);
+    FormCount++;
 }
 
 function DeleteForm(){
-    for(let i = 0; i < 2; i++){
-        var DeleteCount = FormCount-1;
-        var parent = document.getElementById('form_area');
-        var target = document.getElementById('inputform' + DeleteCount);
-        target.remove();
-        clanname.pop();
-        FormCount--;
-    }
+    var DeleteCount = FormCount-1;
+    var parent = document.getElementById('form_area');
+    var target = document.getElementById('inputform' + DeleteCount);
+    target.remove();
+    clanname.pop();
+    FormCount--;
 }
 
 function WeekMatch(w, firstnum, secondnum){
@@ -31,36 +27,45 @@ function WeekMatch(w, firstnum, secondnum){
     return ret;
 }
 
-function WeekMatchSample(w, firstnum, secondnum){
-    ret = "<li class=\"match\"><div>" + clanname[firstnum] + "</div>";
-    ret += "<div>draw</div>";
-    ret += "<div>" + clanname[secondnum] + "</div></li>";
-    return ret;
-}
-
-function ClanName(){
+function ClanName(){ // 我ながらこの書き方はどうかと思おう（が、とりあえず使えるようにしたいので放置）
     var schedule = document.getElementById('schedule');
     var inner = ""
     for(let i = 0; i < FormCount; i++){
         const clan = document.getElementById('inputform'+i);
         clanname[i] = clan.value;
     }
-    for(let i = 1; i < FormCount; i++){
-        inner += "<li><ul class=\"week\"><p>week" + i + "<p>";
-        inner += WeekMatch(i, 0, i);
-        var firstclan = i+1, secondclan = FormCount+i-2;
-        if(secondclan >= FormCount) secondclan = secondclan%FormCount+1; 
-        for(var j = 0; j < FormCount/2-1; j++, firstclan++, secondclan--){
-            if(firstclan == FormCount) firstclan = 1;
-            if(secondclan == 0) secondclan = FormCount-1;
-            inner += WeekMatch(i, firstclan, secondclan);
+    if(FormCount%2 == 0){
+        for(let i = 1; i < FormCount; i++){
+            inner += "<li><ul class=\"week\"><p>week" + i + "<p>";
+            inner += WeekMatch(i, 0, i);
+            var firstclan = i+1, secondclan = FormCount+i-2;
+            if(secondclan >= FormCount) secondclan = secondclan%FormCount+1; 
+            for(var j = 0; j < FormCount/2-1; j++, firstclan++, secondclan--){
+                if(firstclan == FormCount) firstclan = 1;
+                if(secondclan == 0) secondclan = FormCount-1;
+                inner += WeekMatch(i, firstclan, secondclan);
+            }
+            inner += "</ul></li>";
+            var input = document.createElement('li');
+            input.innerHTML = inner;
+            schedule.appendChild(input);
+            inner = ""
         }
-        inner += "</ul></li>";
-        var input = document.createElement('li');
-        input.innerHTML = inner;
-        schedule.appendChild(input);
-        inner = ""
+    } else {
+        for(var week = 1; week <= FormCount; week++){
+            inner += "<li><ul class=\"week\"><p>week" + week + "<p>";
+            for(var i = 0; i < (FormCount-1)/2; i++){
+                inner += WeekMatch(week, (i+week-1)%FormCount, (2*FormCount-i-2+week)%FormCount);
+                console.log((2*FormCount-i-2+week)%FormCount);
+            }
+            inner += "</ul></li>";
+            var input = document.createElement('li');
+            input.innerHTML = inner;
+            schedule.appendChild(input);
+            inner = ""
+        }
     }
+
     var input = document.createElement('div');
     inner = "<input id = \"ok\" type=\"button\" value=\"OK\" onclick=\"Calc()\"></input>"
     input.innerHTML = inner;
@@ -105,7 +110,7 @@ function Calc() {
         values[0].push(1);
         values[1].push(1);
     }
-    for(var i = 1; i < FormCount; i++){
+    for(var i = 1; i < FormCount+(FormCount%2); i++){
         for(var j = 0; j < win[i].length; j++){
             winner = win[i][j].split('-')[0];
             loser = win[i][j].split('-')[1];
@@ -115,7 +120,7 @@ function Calc() {
             values[0][loser] = loser_value - (1/(winner_value*(2*(FormCount-1))));
         }
     }
-    for(var i = FormCount-1; i > 0; i--){
+    for(var i = FormCount-1+(FormCount%2); i > 0; i--){
         for(var j = 0; j < win[i].length; j++){
             winner = win[i][j].split('-')[0];
             loser = win[i][j].split('-')[1];
@@ -139,11 +144,4 @@ function Calc() {
     input.innerHTML = inner;
     result.appendChild(input);
     document.getElementById('ok').remove();
-}
-
-function WeekMatch(w, firstnum, secondnum){
-    ret = "<li class=\"match\"><div id=\"Week" + w + "Clan"+ firstnum + "-" + secondnum + "\" onclick=\"Win()\">" + clanname[firstnum] + "</div>";
-    ret += "<div id = \"Week" + w + "Draw" + firstnum + "-"+ secondnum + "\" onclick=\"Win()\">draw</div>";
-    ret += "<div id=\"Week" + w + "Clan"+ secondnum + "-" + firstnum + "\" onclick=\"Win()\">" + clanname[secondnum] + "</div></li>";
-    return ret;
 }
