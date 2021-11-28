13961b51f1fa64bb2914cba51420ba1a75134b6b
mikecat1024 <akkt.mikecat@outlook.jp>
2021-11-28 18:39:44 +0900

    完成

--- js/newindex.js
+++ js/newindex.js
@@ -8,27 +8,33 @@ function add_clan(){
 }
 
 function subtract_clan(){
+    if(clan_number == 2){
+        alert("Set the number of clans to be greater than one.")
+        return 0
+    }
     clan_number--
     document.getElementById("clan-number").textContent = clan_number
 }
 
 function input_HTML(week, match, str){
-    var HTML = "<input type=\"text\" id=\"week" + week + "match" + match + str + "\">"
+    var HTML = "<input type=\"text\" id=\"week" + week + "match" + match + str + "\" placeholder=\"clan name\">"
     return HTML
 }
 
 function match_HTML(week, match){
     var HTML = "<li class=\"match\">"
-        HTML += "<div>" + input_HTML(week, match, "-winner") + "</div>"
-        HTML += "<div id=\"week" + week + "match" + match + "\" class=\"defeat\" onclick=\"change_tie_defeat()\">defeats</div>"
-        HTML += "<div>" + input_HTML(week, match, "-loser") + "</div>"
+        HTML += "<ul>"
+            HTML += "<li>" + input_HTML(week, match, "-winner") + "</li>"
+            HTML += "<li id=\"week" + week + "match" + match + "\" class=\"defeat\" onclick=\"change_tie_defeat()\">defeats</li>"
+            HTML += "<li>" + input_HTML(week, match, "-loser") + "</li>"
+        HTML += "</ul>"
     HTML += "</li>"
     return HTML
 }
 
 function week_HTML(week){
     var HTML = "<p>week" + week + "</p>"
-    HTML += "<ul id=\"week" + week + "\">"
+    HTML += "<ul id=\"week" + week + "\" class=\"week-unit\">"
     for(var match = 0; match < match_number; match++){
         HTML += match_HTML(week, match)
     }
@@ -40,21 +46,23 @@ function set_button(){
     document.getElementById("calc-button-area").classList.remove("hidden")
 }
 
-function reset_schedule(){
-    var schedule = document.getElementById("schedule")
+function reset(id){
+    var schedule = document.getElementById(id)
     var empty = schedule.cloneNode(false)
     schedule.parentNode.replaceChild(empty, schedule)
 }
 
 function set_schedule(){
     set_button()
-    reset_schedule()
+    reset("schedule")
     week_number = clan_number-!(clan_number%2)
     match_number = Math.floor(clan_number/2)
 
     var HTML = document.createElement('ul')
+    HTML.id="weeks"
     for(var week = 1; week <= week_number; week++){
-        HTML.innerHTML += "<li>" + week_HTML(week) + "</li>"
+        HTML.innerHTML += "<li class=\"week\">" + week_HTML(week) + "</li>"
+        if(week%4 == 0) HTML.innerHTML += "<br>"
     }
     document.getElementById("schedule").appendChild(HTML);
 }
@@ -74,12 +82,45 @@ function get_name_map(){
     return map
 }
 
-function check_name(){
+function validate(){
     var clan_name = get_name_map()
+
+    if(Object.keys(clan_name).includes("")){
+        alert("Enter the name of clans")
+        return false
+    }
+
+    var matches = {}
+    for(var week = 1; week <= week_number; week++){
+        var clans = {}
+        for(var match = 0; match < match_number; match++){
+            var winner_id = "week" + week + "match" + match + "-winner"
+            var loser_id = "week" + week + "match" + match + "-loser"
+            var winner = document.getElementById(winner_id)
+            var loser = document.getElementById(loser_id)
+            if(winner.value == loser.value){
+                alert("Winner is equal to loser.")
+                return false
+            }
+            clans[winner.value] = 1
+            clans[loser.value] = 1
+            matches[winner.value + '-' + loser.value] = 1
+        }
+        if(Object.keys(clans).length != 2*match_number){
+            alert("Some clans are duplicated.")
+            return false
+        }
+    }
+    if(Object.keys(matches).length != week_number*match_number){
+        alert("Some matches are duplicated.")
+        return false
+    }
+
     if(Object.keys(clan_name).length != clan_number){
         alert("the numbers of clan is not equal to the number of names of clan")
         return false
-    } else {
+    }
+    else {
         return true
     }
 }
@@ -91,7 +132,10 @@ function is_ties(id){
 }
 
 function set_result(start_values, end_values){
+    reset('result-area')
     HTML  = document.createElement('div')
+    HTML.id = "result"
+    HTML.innerHTML += "<p id=\"result-title\">B-value</p>"
     for(var i = 0; i < clan_number; i++){
         clan =  Object.keys(start_values)[i]
         HTML.innerHTML += "<p>" + clan + " : " + (start_values[clan] + end_values[clan])/2 + "</p>"
@@ -100,7 +144,7 @@ function set_result(start_values, end_values){
 }
 
 function calc_value(){
-    // if(!check_name()) return 0
+    if(!validate()) return 0
     var start_values = get_name_map()
     var end_values = get_name_map()
     for(var week = 1; week <= week_number; week++){
